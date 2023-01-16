19ec76ece7c77a370a72861a292fcbb93d9bd936
Rob74starogard <92817159+Rob74starogard@users.noreply.github.com>
2023-01-16 08:55:19 +0100

    matplotlib -> out

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import altair as alt
 import zipfile
 from meteostat import Point, Daily
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+
 
 def read_data(file):
     pomiary = zipfile.ZipFile(file+'.zip','r')
@@ -48,6 +52,9 @@ if __name__=='__main__':
     dane_pog = dane_pogodowe(output)
     print(output)
     data2share=output.merge(dane_pog, how='inner', on='time')
+    data2share.rename(columns = {'time':'Date'
+                                ,'usage':'Power consumption[kWh]'}, inplace = True)
+    
     #print(data2share)
     st.title('Heat pump power consumption as a function of environmental variables')
     with st.expander('About...'):
@@ -59,12 +66,106 @@ if __name__=='__main__':
     if st.button('Show input dataframe'):
         st.dataframe(data2share)
     
-    c = alt.Chart(data2share).mark_circle().encode(x='tmin', y='usage', size='usage', color='usage', tooltip=['time', 'usage']).properties(
-    title='Summary data')
-    st.write(c)
-    data_nov=data2share.loc[(data2share['time']>='2022-11-1')&(data2share['time']<='2022-11-30')]
-    d = alt.Chart(data_nov).mark_circle().encode(x='tmin', y='usage', size='usage', color='usage', tooltip=['time', 'usage']).properties(
-    title='November data')
-    st.write(d)
-        
-        
+
+    data_nov=data2share.loc[(data2share['Date']>='2022-11-1')&(data2share['Date']<='2022-11-30')]
+    data_dec=data2share.loc[(data2share['Date']>='2022-12-1')&(data2share['Date']<='2022-12-31')]
+    data_jan=data2share.loc[(data2share['Date']>='2023-01-1')&(data2share['Date']<'2023-01-15')]
+    
+
+    rozmiar=25
+    
+    fig = px.scatter(
+        data_nov,
+        x="tmin",
+        y="Power consumption[kWh]",
+        size="Power consumption[kWh]",
+        color="Power consumption[kWh]",
+        title='Summary data power usage',
+        labels={
+                     "tmin": "Minimal temperature [deg C]"},
+        size_max=rozmiar,
+    )
+    st.plotly_chart(fig, theme='streamlit', use_container_width=True)
+
+    fig = px.scatter(
+        data_dec,
+        x="tmin",
+        y="Power consumption[kWh]",
+        size="Power consumption[kWh]",
+        color="Power consumption[kWh]",
+         #hover_name="country",
+        size_max=rozmiar,
+        title="December data power usage",
+        labels={"tmin": "Minimal temperature [deg C]"})
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+    
+    fig = px.scatter(
+        data_jan,
+        x="tmin",
+        y="Power consumption[kWh]",
+        size="Power consumption[kWh]",
+        color="Power consumption[kWh]",
+        title='January data power usage',
+        size_max=rozmiar,
+        labels={
+                     "tmin": "Minimal temperature [deg C]"}
+    )
+
+    st.plotly_chart(fig, theme='streamlit', use_container_width=True)
+
+
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+        name='Minimal 24h temperature',
+        x=data_jan['Date'],
+        y=data_jan['tmin'],
+        #color=data_jan['tmin']
+        ))
+    
+    fig.add_trace(
+        go.Bar(
+        name="Power consumption[kWh]",
+        x=data_jan['Date'],
+        y=data_jan["Power consumption[kWh]"]
+        )
+    )
+    fig.update_layout(
+    title="Power consumption in January 2023 as function of min temperature",
+    xaxis_title="Date",
+    #yaxis_title="Y Axis Title",
+    legend_title="Data descripion",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="RebeccaPurple"))
+    st.plotly_chart(fig, theme='streamlit', use_container_width=True)
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+        name='Minimal 24h temperature',
+        x=data2share['Date'],
+        y=data2share['tmin'],
+        #color=data_jan['tmin']
+        ))
+    
+    fig.add_trace(
+        go.Bar(
+        name="Power consumption[kWh]",
+        x=data2share['Date'],
+        y=data2share["Power consumption[kWh]"]
+        )
+    )
+    fig.update_layout(
+    title="Power consumption in whole heating period as function of min 24h temperature",
+    xaxis_title="Date",
+    #yaxis_title="Y Axis Title",
+    legend_title="Data descripion",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="RebeccaPurple"))
+    st.plotly_chart(fig, theme='streamlit', use_container_width=True)
